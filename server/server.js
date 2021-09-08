d681088980299382fee9f8f98b823af2218783ee
Andile-coder <maselaandile@gmail.com>
2021-09-08 21:19:52 +0200

    server file

--- server/server.js
+++ server/server.js
@@ -1,15 +1,53 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// Store and retrieve your videos from here
-// If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
-
-// GET "/"
-app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: "andilemasela",
+  host: "localhost",
+  database: "cyf_videos",
+  password: "Masela@2018",
+  port: 5432,
 });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+
+  next();
+});
+app.use(express.json());
+app.get("/", (req, res) => {
+  pool.query("SELECT * FROM videos", (error, result) => {
+    res.json(result.rows);
+  });
+});
+app.post("/", (req, res) => {
+  const title = req.body.title;
+  const url = req.body.url;
+  const rating = 0;
+  const query = `INSERT INTO videos (title,url,rating) VALUES ($1,$2,$3)`;
+  pool.query(query, [title, url, rating]).catch((e) => console.error(e));
+  console.log(req.body.title);
+});
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  pool.query(`SELECT * FROM videos WHERE id=${id}`, (error, result) => {
+    res.json(result.rows);
+  });
+});
+app.delete("/:id", (req, res) => {
+  pool
+    .query(`DELETE FROM videos WHERE id=${req.params.id}`)
+    .catch((e) => console.error(e));
+});
+app.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const rate = req.body.rating;
+  pool
+    .query(`UPDATE videos SET rating=$1 WHERE ID=$2`, [rate, id])
+    .then(() => console.log(rate))
+    .catch((e) => console.error(e));
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
